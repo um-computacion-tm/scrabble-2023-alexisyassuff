3d86c1c83fac1acec61252e177089e1f619966a7
Alexis <a.yassuff@alumno.um.edu.ar>
2023-08-29 14:46:09 -0300

    Creacion de tests y funciones encargadas de verificacion de fichas del jugador y realizacion del restado cuando esto sea satisfactorio

--- game/player.py
+++ game/player.py
@@ -2,19 +2,31 @@ class Player:
     def __init__(self):
         self.tiles = []
         
-    def hacer_jugada(self, palabra):
-        letras = list(palabra) #Deletrea la palabra ingresada por usuario
-        tiles_duplicado = self.tiles.copy() #Hago una duplicacion de las fichas del jugador para la verificacion
-    #Y no borrar de la lista de fichas del jugador hasta que la coincidencia sea total
-    
-        for l in letras: #Recorro la palabra del usuario deletreada 
-            if l in tiles_duplicado: 
-                tiles_duplicado.remove(l) #La elimino de la lista si coinciden
-            else:
-                print("No posee fichas en su mano") 
-                return #Si no coincide en alguna letra: salta
-        return palabra # retorna la palabra ingresada corrrectamente si existio coincidencia siempre
+    def make_play(self, word): 
+        if self.verify_tiles_in_hand(word): 
+            #Si la funcion verificar devuelve True: 
+                self.subtract_tiles(word) #Se va a restar de las fichas del player
+                return word #Y retornara la palabra ingresada satisfactoriamente
+            #Si devuelve False significa que hubo al menos una ficha que no tenia en su haber
+        else: 
+                print("No posee fichas en su mano")
+                return
             
-        
-        
     
+    def verify_tiles_in_hand(self, word):  #Verificacion fichas en mano devolvera:
+            letter = list(word) 
+            tiles_duplicado = self.tiles.copy() 
+            for l in letter:
+                if l in tiles_duplicado:
+                    tiles_duplicado.remove(l)
+                else:
+                    print("Falta ficha ", l)
+                    return False         #False si alguna ficha no esta en la palabra deseada
+            return True                 #True si todas sus fichas estan en la palabra deseada
+    
+    def subtract_tiles(self, word): #Funcion especificamente de restar fichas para dividir en pequeñas tareas
+            letter = list(word)     #Y tener posibilidad de reutilizar el codigo luego con esta funcion
+            for l in letter:
+                self.tiles.remove(l)
+            return
+
--- test/test_player.py
+++ test/test_player.py
@@ -11,14 +11,21 @@ class TestPlayer(unittest.TestCase): #Objetivo revisar que usuario comience con 
         )
     
      
-    def test_hacer_jugada(self): #Objetivo 
+    def test_make_play_valid_word(self): #Objetivo camino palabra ingresada correctamente
         player_1 = Player()
-        player_1.tiles = ["h", "o", "l", "a"]
-        palabra = player_1.hacer_jugada("hola") 
-        self.assertEquals(palabra, 
+        player_1.tiles = ["h", "o", "l", "a", "b", "j"] #Fichas jugador 
+        word = player_1.make_play("hola")  #Llamado a funcion con word = hola
+        self.assertEquals(word,  
     "hola"        
         )
-        
+        self.assertEqual(len(player_1.tiles), 2) #Se restan 4 fichas "hola" a 6 fichas 
+    
+    def test_make_play_not_valid_word(self): #Camino no valido 
+        player_1 = Player()
+        player_1.tiles = ["h", "o", "l", "a", "b", "j"] 
+        word = player_1.make_play("hacer") #Cuando alguna letra no es en sus fichas retorna None
+        self.assertEqual(word, None) 
+        self.assertEqual(len(player_1.tiles), 6) #Las fichas se mantienen iguales
 
 if __name__ == '__main__':
     unittest.main()
