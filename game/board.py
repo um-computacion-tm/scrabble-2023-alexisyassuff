22adbc15804753f3782b0f4792aeb9347ab24ffc
Alexis <a.yassuff@alumno.um.edu.ar>
2023-09-12 23:04:46 -0300

    Methods that run the respective tests were added to the Board class, checking the class to see if a word fits on the game board

--- game/board.py
+++ game/board.py
@@ -8,4 +8,24 @@ class Board:
             for _ in range(15)
         ]
     
-   
+    def validate_word_inside_board(self, word, location, orientation):
+        row, col = location
+
+    def validate_word_inside_board(self, word, location: tuple, orientation):
+        horizontal_position = location[0]
+        vertical_position = location[1]
+        len_word = len(word)
+        orientation = orientation.upper()
+      
+        if orientation == "V":
+            if vertical_position + len_word > 15:
+                return False
+            elif horizontal_position + len_word > 1:
+                return True 
+        elif orientation == "H":
+            if horizontal_position + len_word > 15:
+                return False
+            elif vertical_position + len_word > 1:
+                return True 
+    
+    
